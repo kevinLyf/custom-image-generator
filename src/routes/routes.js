ac2c1c2b1a31d2410d1a13b94c08b72a234038b5
Kevin Feitosa <kevin.feitosa11@outlook.com>
2023-06-08 19:09:16 -0300

    feat: add meme route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import letter from "./letter/letter.js";
+import meme from "./meme/meme.js";
 
 const router = Router();
 
 router.use(letter);
+router.use(meme);
 
 export default router;
